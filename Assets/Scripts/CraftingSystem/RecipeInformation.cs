fefe52b320faf2a36c619005fef2b1a55d1f8157
Kovinejtor <95875142+Kovinejtor@users.noreply.github.com>
2024-05-15 11:36:07 +0200

    Create the resource folder and minor changes about the connection betwen two scenes

--- Assets/Scripts/CraftingSystem/RecipeInformation.cs
+++ Assets/Scripts/CraftingSystem/RecipeInformation.cs
@@ -6,7 +6,16 @@ namespace CraftingSystem
     public class RecipeInformation : MonoBehaviour
     {
         public RecipeData recipeData;
+
         public Image craftingSlotQImage;
+        public Image craftingSlotWImage;
+        public Image craftingSlotEImage;
+
+        public Image ResultImage;
+        public Image FirstIngridientImage;
+        public Image SecondIngridientImage;
+        public Image ThirdIngridientImage;
+
         private void Start()
         {
             // Ensure that the RecipeData is assigned before using it
@@ -24,8 +33,16 @@ namespace CraftingSystem
             Debug.Log("First Ingredient Sprite Name: " + recipeData.firstIngredientSpriteName);
             Debug.Log("Second Ingredient Sprite Name: " + recipeData.secondIngredientSpriteName);
             Debug.Log("Third Ingredient Sprite Name: " + recipeData.thirdIngredientSpriteName);
-            var loadedSprite = Resources.Load<Sprite>(recipeData.firstIngredientSpriteName);
-            craftingSlotQImage.sprite = loadedSprite;
+
+            craftingSlotQImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.firstIngredientSpriteName);
+            craftingSlotWImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.secondIngredientSpriteName);
+            craftingSlotEImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.thirdIngredientSpriteName);
+
+            ResultImage.sprite = Resources.Load<Sprite>("Potions/" + recipeData.potionSpriteName);
+            FirstIngridientImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.firstIngredientSpriteName);
+            SecondIngridientImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.secondIngredientSpriteName);
+            ThirdIngridientImage.sprite = Resources.Load<Sprite>("Plants/" + recipeData.thirdIngredientSpriteName);
+
         }
     }
 }
