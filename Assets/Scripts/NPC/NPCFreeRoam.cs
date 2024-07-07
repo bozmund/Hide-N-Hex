7a5e9d653b3a43c5d1e27f0a9e5b4902121ccb8e
Mattia Grubiša <mattia.grubisa@gmail.com>
2024-07-07 13:00:05 +0200

    Add Buć to cabin scene

--- Assets/Scripts/NPC/NPCFreeRoam.cs
+++ Assets/Scripts/NPC/NPCFreeRoam.cs
@@ -4,75 +4,48 @@ using UnityEngine;
 public class NPCFreeRoam : MonoBehaviour
 {
     public float moveSpeed = 1f;
-
-    public float minX = -2f;
-    public float maxX = 2f;
-    public float minY = -2f;
-    public float maxY = 2f;
+    private float minX = -2f;
+    private float maxX = 2f;
+    private float minY = -2f;
+    private float maxY = 2f;
+    public bool isMoving = true;
 
     private Vector2 targetPosition;
     private Vector2 _villagePosition;
-    private bool isMoving = true;
+
+    public Rigidbody2D rb2d;
 
     void Start()
     {
-        _villagePosition = transform.position;
+        _villagePosition = new Vector2(71, 8);
         SetRandomTargetPosition();
-        StartCoroutine(MoveAndWait());
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Ouch");
-        isMoving = false;
-        StartCoroutine(HandleCollision());
     }
 
-    private IEnumerator HandleCollision()
-    {
-        yield return new WaitForSeconds(Random.Range(1f, 3f));
-        SetRandomTargetPosition();
-        isMoving = true;
-    }
-
-    private IEnumerator MoveAndWait()
+    public IEnumerator MoveAndWait()
     {
         while (true)
         {
             if (isMoving)
             {
-                MoveTowardsTarget();
+                rb2d.FreeRoam(targetPosition, moveSpeed);
 
                 if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
                 {
                     isMoving = false;
+                    rb2d.velocity = Vector2.zero;
                     yield return new WaitForSeconds(Random.Range(1f, 3f));
                     SetRandomTargetPosition();
                     isMoving = true;
                 }
             }
-
             yield return null;
         }
     }
 
-    void SetRandomTargetPosition()
+    public void SetRandomTargetPosition()
     {
-        do
-        {
-            float randomX = Random.Range(minX, maxX);
-            float randomY = Random.Range(minY, maxY);
-            targetPosition = new Vector2(_villagePosition.x + randomX, _villagePosition.y + randomY);
-        } while (!IsWithinBounds(targetPosition));
-    }
-
-    bool IsWithinBounds(Vector2 position)
-    {
-        return position.x >= _villagePosition.x + minX && position.x <= _villagePosition.x + maxX &&
-               position.y >= _villagePosition.y + minY && position.y <= _villagePosition.y + maxY;
-    }
-
-    void MoveTowardsTarget()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        targetPosition = new Vector2(_villagePosition.x + randomX, _villagePosition.y + randomY);
     }
 }
---
+++ Assets/Scripts/NPC/PlantListForCat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantListForCat
+{
+    public enum PlantsForCat{
+        firebloom, 
+        icecap, 
+        earthroot, 
+        blindweed, 
+        rotberry, 
+        mageroyal, 
+        sungrass, 
+        sorrowmoss, 
+        swiftthistle
+    }
+}
